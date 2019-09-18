36a0449a1928045bc2314f725b94eb2c67da6cac
chenxuan <523764720@qq.com>
2019-09-18 18:39:32 +0800

    增加对通讯频道的控制

--- src/Main_ui.ts
+++ src/Main_ui.ts
@@ -1,11 +1,13 @@
 import { UI } from "electron_commandline_UI";
 import _ from "lodash";
 import { Command_helper } from "./Command_helper";
+import { Config_helper } from "./Config_helper";
 import { isUndefined } from "util";
 
 export class Main_ui extends UI
 {
     command_helper: Command_helper
+    conf: Config_helper
 
     constructor(win_setting?: object | undefined)
     {
@@ -14,6 +16,7 @@ export class Main_ui extends UI
         }
         super(_.merge(preset, win_setting))
         this.command_helper = new Command_helper()
+        this.conf = new Config_helper()
     }
 
     async init_win(_option?: { cmd_text: string, cmd_title: string})
@@ -49,6 +52,29 @@ export class Main_ui extends UI
                 }
             }, 1e3)
             return "关闭中......"
+        })
+        this.init_command_channel()
+    }
+
+    init_command_channel()
+    {
+        this.command_helper.add_func("set_channel", async (name: string) =>
+        {
+            this.conf.set("current_channel", name)
+            return "设置通讯频道成功"
+        })
+        this.command_helper.add_func("get_channel", async () =>
+        {
+            let cmd_return: string
+            try
+            {
+                cmd_return = this.conf.get("current_channel")
+            }
+            catch(e)
+            {
+                cmd_return = "尚未设置通讯频道"
+            }
+            return cmd_return
         })
     }
 }
