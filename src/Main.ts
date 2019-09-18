790bc02d5dbe5360a1f5f526e9fb2b9b20fa3725
chenxuan <523764720@qq.com>
2019-09-18 14:33:11 +0800

    更新badbadconnection, 现在不会c端发送消息自己又重复收到了

--- src/Main.ts
+++ src/Main.ts
@@ -8,8 +8,10 @@ app.on("ready", async() =>
     let c1 = new Client("mysaa")
     let s1 = new Server("mysaa")
     c1.set_password("cx",2)
-    c1.on_resv = () => {}
     await s1.start()
     await c1.start()
-    c1.send("222")
+    setInterval(() =>
+    {
+        c1.send(String(Date.now() + Math.random()* 1e10))
+    },1e3)
 })
