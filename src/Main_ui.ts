76e4f9bc2d6bb3b1b8cb733fbf235b74f64cf459
chenxuan <523764720@qq.com>
2019-09-18 16:59:03 +0800

    隐藏菜单栏

--- src/Main_ui.ts
+++ src/Main_ui.ts
@@ -1,6 +1,17 @@
 import { UI } from "electron_commandline_UI";
+import _ from "lodash";
 
 export class Main_ui extends UI
 {
-    
+    constructor(win_setting?: object | undefined)
+    {
+        let preset = {
+            autoHideMenuBar: true
+        }
+        super(_.merge(preset, win_setting))
+    }
+    async init_win(_option?: { cmd_text: string; cmd_title: string;})
+    {
+        await super.init_win(_option)
+    }
 }
