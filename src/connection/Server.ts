43d477a3113ffc28ee4c7b840938d1891cbe0a03
chenxuan <523764720@qq.com>
2019-09-20 15:14:48 +0800

    server存放一个子进程池, 在子进程依旧运行的情况下, 是不会关闭的

--- src/connection/Server.ts
+++ src/connection/Server.ts
@@ -1,9 +1,18 @@
 import { Connection_base } from "./Connection_base";
-import { spawn } from "child_process";
+import { spawn, ChildProcess } from "child_process";
 import { decode } from "iconv-lite";
+import _ from "lodash";
 
 export class Server extends Connection_base
 {
+    cmd_process_list: ChildProcess[]
+
+    constructor(name: string)
+    {
+        super(name)
+        this.cmd_process_list = []
+    }
+
     on_resv(msg: string)
     {
         console.log(`server_recv: ${msg}`);
@@ -25,12 +34,17 @@ export class Server extends Connection_base
     run_cmd(cmd: string)
     {
         let terminal = spawn(cmd, {shell: true})
+        this.cmd_process_list.push(terminal)
         terminal.stdout.on("data", (data: Buffer) =>
         {
             if(data)
             {
                 this.cmd_output(data)
             }
+        })
+        terminal.stdout.on("end", () =>
+        {
+            _.pull(this.cmd_process_list, terminal)
         })
     }
 }
