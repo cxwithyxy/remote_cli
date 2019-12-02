8087b18f4addf97d72e74e545490494c44dd9b95
chenxuan <523764720@qq.com>
2019-12-02 16:51:25 +0800

    下载功能可用了

--- cli_tool/check_terminal_http_api_exe.ts
+++ cli_tool/check_terminal_http_api_exe.ts
@@ -1,10 +1,6 @@
 import pathExists from "path-exists";
-import axios from "axios";
-
-async function net_get(url: string)
-{
-    return (await axios.get(url)).data
-}
+import { net_get, download_it, display_download_file_size } from "./src/func_api";
+import { unlinkSync } from "fs";
 
 (async () =>
 {
@@ -15,12 +11,24 @@ async function net_get(url: string)
 
     if(!(has_exe && has_node_modules))
     {
-        console.log("terminal_http_api EXE NOT FOUND");
-        console.log("download terminal_http_api EXE now");
+        let terminal_http_api_zip_path = `${__dirname}/../terminal_http_api.zip`
+        if(await pathExists(terminal_http_api_zip_path))
+        {
+            unlinkSync(terminal_http_api_zip_path)
+        }
+        console.log("# terminal_http_api EXE NOT FOUND");
+        console.log("# download terminal_http_api EXE now");
         let all_releases = await net_get("https://api.github.com/repos/cxwithyxy/terminal_http_api/releases/latest")
         let latest_release = await net_get(all_releases.assets_url)
         console.log(latest_release[0].browser_download_url)
-        
-        
+        let stop_display_dl = display_download_file_size(terminal_http_api_zip_path, 300, (now_size) =>
+        {
+            // console.log(`${now_size}/${latest_release[0].size}`);
+            let p = (now_size/83614240 /* latest_release[0].size */ * 100).toFixed(2)
+            console.log(`${p}%`);
+            
+        })
+        await download_it(/* latest_release[0].browser_download_url */"https://qd.myapp.com/myapp/qqteam/pcqq/PCQQ2019.exe", terminal_http_api_zip_path)
+        stop_display_dl()
     }
 })()
---
+++ cli_tool/src/func_api.ts
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { Log, Options, DownloadTarget, Wgdown } from "wgdown";
+import { statSync } from "fs";
+
+export async function net_get(url: string)
+{
+    return (await axios.get(url)).data
+}
+
+
+export function display_download_file_size(path: string, looptime: number, display_call: (current_size: number, stop_func?: () => void) => void ): () => void
+{
+
+    let running = true
+    let handle: NodeJS.Timeout
+    let last_size = 0
+
+    function stop()
+    {
+        clearInterval(handle)
+        running = false
+    }
+    
+    handle = setInterval(() =>
+    {
+        if(running)
+        {
+            let size = 0
+            try
+            {
+                size = statSync(path).size
+            }
+            catch(e){}
+            if(last_size != size)
+            {
+                display_call(size)
+                last_size = size
+            }
+            
+        }
+    }, looptime)
+
+    return stop
+}
+
+export async function download_it(url: string, download_path:string)
+{
+    await new Promise(succ =>
+    {
+        let options = <Options>{}
+        options.list = [<DownloadTarget>{serverPath: url, localPath: download_path}]
+        options.cpus = 4
+        options.errorLimit = 2
+        options.quiet = true
+        options.callback = (log: Log, errorList: Array<string>)=>{
+            console.log("download finish");
+            succ()
+        }
+        let wgdown: Wgdown = new Wgdown(options);
+        wgdown.download();
+    })
+}
